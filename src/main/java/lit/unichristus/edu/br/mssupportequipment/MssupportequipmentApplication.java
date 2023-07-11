7813234debf9c96ff626e93dcadc4947b70f4052
ERIK <42251716+gerikpng@users.noreply.github.com>
2023-07-11 18:28:53 -0300

    first commit

---
+++ src/main/java/lit/unichristus/edu/br/mssupportequipment/MssupportequipmentApplication.java
@@ -0,0 +1,13 @@
+package lit.unichristus.edu.br.mssupportequipment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MssupportequipmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MssupportequipmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lit/unichristus/edu/br/mssupportequipment/MssupportequipmentApplicationTests.java
@@ -0,0 +1,13 @@
+package lit.unichristus.edu.br.mssupportequipment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MssupportequipmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
